eb417f54f900405552bec8e4e4e20c85a1f4a8e8
Navneet Desai <navneetdesai44@gmail.com>
2023-05-31 18:01:06 +0530

    Refactor: Remove duplicate code by creating fixture for default openai model

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -5,15 +5,18 @@ from pypixel.models import OpenAI
 
 
 class TestOpenAI:
-    def test_default_params(self):
-        model = OpenAI()
-        assert model.engine == "text-davinci-003"
-        assert model.max_tokens == 1200
-        assert model.temperature == 0
-        assert model.top_p == 1
-        assert model.frequency_penalty == 0.25
-        assert model.presence_penalty == 0
-        assert not hasattr(model, "api_key")
+    @pytest.fixture(autouse=True)
+    def default_model(self):
+        return OpenAI()
+
+    def test_default_params(self, default_model):
+        assert default_model.engine == "text-davinci-003"
+        assert default_model.max_tokens == 1200
+        assert default_model.temperature == 0
+        assert default_model.top_p == 1
+        assert default_model.frequency_penalty == 0.25
+        assert default_model.presence_penalty == 0
+        assert not hasattr(default_model, "api_key")
 
     def test_custom_params(self):
         model = OpenAI(
@@ -33,44 +36,41 @@ class TestOpenAI:
         assert model.presence_penalty == 0.5
         assert not hasattr(model, "api_key")
 
-    def test_type(self):
-        model = OpenAI()
-        assert model.model == "OpenAI"
+    def test_type(self, default_model):
+        assert default_model.model == "OpenAI"
 
-    def test_secrets(self):
-        model = OpenAI()
+    def test_secrets(self, default_model):
         assert hasattr(
-            model, "_secrets"
+            default_model, "_secrets"
         ), "Create a secrets.json file with your OpenAI API key"
         assert openai.api_key is not None
 
-    def test_representation(self):
-        model = OpenAI()
-        assert str(model) == "OpenAI"
-        assert repr(model) == "OpenAI"
+    def test_representation(self, default_model):
+        assert str(default_model) == "OpenAI"
+        assert repr(default_model) == "OpenAI"
 
-    def test_openai_implementation(self, mocker):
-        model = OpenAI()
+    def test_openai_implementation(self, mocker, default_model):
+        default_model = OpenAI()
         prompt = "Generate code for histogram equalization."
         expected_text = "# mock results \n import cv2"
 
         mock = mocker.patch("openai.Completion.create")
         mock.return_value = {"choices": [{"text": expected_text}]}
-        response = model.run(prompt)
+        response = default_model.run(prompt)
         mock.assert_called_once_with(
-            engine=model.engine,
+            engine=default_model.engine,
             prompt=prompt,
-            temperature=model.temperature,
-            max_tokens=model.max_tokens,
-            top_p=model.top_p,
-            frequency_penalty=model.frequency_penalty,
-            presence_penalty=model.presence_penalty,
+            temperature=default_model.temperature,
+            max_tokens=default_model.max_tokens,
+            top_p=default_model.top_p,
+            frequency_penalty=default_model.frequency_penalty,
+            presence_penalty=default_model.presence_penalty,
         )
 
         assert response == expected_text
 
-    def test_openai_api_error(self, mocker):
-        mock, model, prompt = self.setup_mocker(mocker)
+    def test_openai_api_error(self, mocker, default_model):
+        mock, model, prompt = self.setup_mocker(mocker, default_model)
         mock.side_effect = openai.error.APIError("API Error")
         response = model.run(prompt)
         mock.assert_called_once_with(
@@ -85,14 +85,14 @@ class TestOpenAI:
 
         assert response is None
 
-    def setup_mocker(self, mocker):
-        model = OpenAI()
+    def setup_mocker(self, mocker, default_model):
+        default_model = OpenAI()
         prompt = "Generate code for histogram equalization."
         mock = mocker.patch("openai.Completion.create")
-        return mock, model, prompt
+        return mock, default_model, prompt
 
-    def test_openai_api_connection_error(self, mocker):
-        mock, model, prompt = self.setup_mocker(mocker)
+    def test_openai_api_connection_error(self, mocker, default_model):
+        mock, model, prompt = self.setup_mocker(mocker, default_model)
         mock.side_effect = openai.error.APIConnectionError("API Connection Error")
         response = model.run(prompt)
         mock.assert_called_once_with(
@@ -107,8 +107,8 @@ class TestOpenAI:
 
         assert response is None
 
-    def test_openai_api_rate_limit_error(self, mocker):
-        mock, model, prompt = self.setup_mocker(mocker)
+    def test_openai_api_rate_limit_error(self, mocker, default_model):
+        mock, model, prompt = self.setup_mocker(mocker, default_model)
         mock.side_effect = openai.error.RateLimitError("Rate Limit Error")
         response = model.run(prompt)
         mock.assert_called_once_with(
